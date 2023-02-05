b8464a5710acec3e83bb7550fb37324bf5647779
Kit64 <81798660+Leaderofepic134@users.noreply.github.com>
2023-02-05 13:14:06 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+const CharacterAI = require('node_characterai');
+const uuid = require('uuid')
+const characterAI = new CharacterAI();
+const prompt = require('prompt-sync')();
+
+
+(async() => {
+    await characterAI.authenticateAsGuest();
+
+    const characterId = "lryij6Pg95hRvpU7y4SLrRzwWvVyCugTF4lpciiJyY8" // Discord moderator
+
+    const chat = await characterAI.createOrContinueChat(characterId);
+    while (true) {
+      const message = prompt('You: ');
+      const response = await chat.sendAndAwaitResponse(message, true)
+      
+      console.log("Orchid: " + response);
+    }
+})();
